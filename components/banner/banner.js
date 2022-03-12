152b41c671827786b9660cb3b26064d53dddbd4d
BensonFeng <bensonfeng@outlook.com>
2022-03-12 16:53:42 -0500

    banner component btn with font

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,14 +1,31 @@
+import styles from "./banner.module.css";
 const Banner = (props) => {
   const { title, subTitle, imgUrl } = props;
+
   const handleOnPlay = () => {
     console.log("handle on play");
   };
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{subTitle}</h2>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>{" "}
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
           backgroundImage: `url(${imgUrl})`,
           width: "100%",
