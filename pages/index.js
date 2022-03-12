55a621e378f184b71bd7653adf8421e6ecfc2e88
BensonFeng <bensonfeng@outlook.com>
2022-03-12 17:35:08 -0500

    structure nav

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
 
 export default function Home() {
   return (
@@ -10,15 +11,17 @@ export default function Home() {
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <NavBar username="test@gmail.com" />
       <Banner
         title="Clifford the Big Red Dog"
         subTitle="big red dog"
         imgUrl="/static/clifford.webp"
       />
+
       {/* <NavBar />
 public\static\clifford.webp
       <Car /> */}
-      <h1>Netflix</h1>
     </div>
   );
 }
