6af9343e485d2391d4808c6256505fa4abd14fa5
BensonFeng <bensonfeng@outlook.com>
2022-03-12 21:11:48 -0500

    navbar router

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,6 +1,22 @@
 import styles from "./navbar.module.css";
 
+import { useRouter } from "next/router";
+
+import Link from "next/link";
+
 const NavBar = (props) => {
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
   const { username } = props;
   return (
     <div className={styles.container}>
@@ -9,8 +25,12 @@ const NavBar = (props) => {
           <div className={styles.logoWrapper}>Netflix</div>
         </a>
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My List</li>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
@@ -21,7 +41,9 @@ const NavBar = (props) => {
 
             <div className={styles.navDropdown}>
               <div>
-                <a className={styles.linkName}>Sign Out</a>
+                <Link href="/about">
+                  <a className={styles.linkName}>Sign Out</a>
+                </Link>
                 <div className={styles.lineWrapper}></div>
               </div>
             </div>
