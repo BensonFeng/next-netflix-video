f5b057ed8b4abc0c64c480cc26bef009d8cd3bf4
BensonFeng <bensonfeng@outlook.com>
2022-03-13 16:52:11 -0400

    implement youtube search api

--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,9 @@ import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
 import SectionCard from "../components/card/section-cards";
 
+import getVideos from "../lib/videos";
 export default function Home() {
-  const disneyVideos = [
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-  ];
+  const disneyVideos = getVideos();
   return (
     <div className={styles.container}>
       <Head>
