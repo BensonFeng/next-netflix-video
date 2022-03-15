0a0ca0f0ed8b29ab1f067dead52e7e45a1cd504a
BensonFeng <bensonfeng@outlook.com>
2022-03-15 15:21:49 -0400

    connect link component video

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,4 +1,5 @@
 import Card from "./card";
+import Link from "next/link";
 
 import styles from "./section-cards.module.css";
 const SectionCards = (props) => {
@@ -8,7 +9,9 @@ const SectionCards = (props) => {
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
         {videos.map((video, idx) => (
-          <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+          <Link href={`/video/${videoId}`}>
+            <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+          </Link>
         ))}
       </div>
     </section>
