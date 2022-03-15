2cf26322840e8858a9106bc2dc68b4aac3528f34
BensonFeng <bensonfeng@outlook.com>
2022-03-15 17:53:44 -0400

    modal structure & style

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,19 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
-
+import clsx from "classname";
 Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
+  const video = {
+    title: "Cute dog",
+    publishTime: "1990-01-01",
+    description: "A big red dog, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 100000,
+  };
+  const { title, publishTime, description, channelTitle, viewCount } = video;
   const {
     query: { videoId },
   } = router;
@@ -28,6 +36,25 @@ const Video = () => {
           src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameborder="0"
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </h1>
   );
