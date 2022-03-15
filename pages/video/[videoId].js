4f976449aee3d5d047b28fdd1a3eeb1adf26762f
BensonFeng <bensonfeng@outlook.com>
2022-03-15 17:14:06 -0400

    modal component styles

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,27 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
   const {
     query: { videoId },
   } = router;
-  return <h1>dynamic video id: {videoId}</h1>;
+  return (
+    <h1 className={styles.container}>
+      dynamic video id: {videoId}
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <div>model body</div>{" "}
+      </Modal>
+    </h1>
+  );
 };
-
 export default Video;
