4daaa97eb1f45a79d9413366dd0f5bc4f6886f41
BensonFeng <bensonfeng@outlook.com>
2022-03-21 19:49:17 -0400

    stats api jwt verification decoded

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,11 +1,15 @@
+import jwt from "jsonwebtoken";
 export default async function stats(req, res) {
   if (req.method === "POST") {
     console.log(req.cookies);
     try {
-      if (!req.cookies.token) {
+      const token = req.cookies.token;
+      if (!token) {
         res.status(403).send();
       } else {
-        res.send({ msg: "it works" });
+        var decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log({ decoded });
+        res.send({ msg: "it works", decoded });
       }
     } catch (error) {
       console.error("Error occurred /stats,", error);
