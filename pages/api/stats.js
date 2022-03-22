9d78d58dd7e578915c86be5c6499e8ba53883502
BensonFeng <bensonfeng@outlook.com>
2022-03-21 20:56:30 -0400

    find user by id graphql

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { findVideoIdByUser } from "../../lib/db/hasura";
 export default async function stats(req, res) {
   if (req.method === "POST") {
     console.log(req.cookies);
@@ -7,9 +8,15 @@ export default async function stats(req, res) {
       if (!token) {
         res.status(403).send();
       } else {
-        var decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log({ decoded });
         res.send({ msg: "it works", decoded });
+        userId = "ankyyyy";
+        videoId = "4zH5iYM4wJo";
+
+        const findVideoId = await findVideoIdByUser(token, userId, videoId);
+        console.log({ findVideoId });
+        res.send({ msg: "it works", decoded, findVideoId });
       }
     } catch (error) {
       console.error("Error occurred /stats,", error);
