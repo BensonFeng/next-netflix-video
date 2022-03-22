10da898ba6a53480455b4018ff89d496275cf300
BensonFeng <bensonfeng@outlook.com>
2022-03-22 15:23:24 -0400

    stats api use insert and update mutation

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
-import { findVideoIdByUser } from "../../lib/db/hasura";
+import { findVideoIdByUser, updateStats } from "../../lib/db/hasura";
 export default async function stats(req, res) {
   if (req.method === "POST") {
     console.log(req.cookies);
     try {
       const token = req.cookies.token;
       if (!token) {
-        res.status(403).send();
+        res.status(403).send({});
       } else {
         const videoId = req.query.videoId;
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
@@ -15,8 +15,16 @@ export default async function stats(req, res) {
         // videoId = "4zH5iYM4wJo";
 
         const findVideoId = await findVideoIdByUser(token, userId, videoId);
-        console.log({ findVideoId });
-        res.send({ msg: "it works", decodedToken, findVideoId });
+        if (findVideoId) {
+          const response = await updateStats(token, {
+            watched: true,
+            userId,
+            videoId: "gxc6y2ZVfCU",
+          });
+          res.send({ msg: "it works", updateStats: response });
+        } else {
+          res.send({ msg: "it works", decodedToken, doesStatsExist });
+        }
       }
     } catch (error) {
       console.error("Error occurred /stats,", error);
