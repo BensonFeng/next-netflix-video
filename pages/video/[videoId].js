bf5c34ff37c1f3a668baf0c99f7e2e3a366107ca
BensonFeng <bensonfeng@outlook.com>
2022-03-22 22:29:58 -0400

    invoke stats api on frontend

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
@@ -43,6 +43,23 @@ const Video = ({ video }) => {
     channelTitle,
     statistics: { viewCount } = { viewCount: 0 },
   } = video;
+
+  useEffect(async () => {
+    const response = await fetch(`/api/stats?videoId=${videoId}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+
+    if (data.length > 0) {
+      const favourited = data[0].favourited;
+      if (favourited === 1) {
+        setToggleLike(true);
+      } else if (favourited === 0) {
+        setToggleDisLike(true);
+      }
+    }
+    console.log({ data });
+  }, []);
 
   const runRatingService = async (favourited) => {
     return await fetch("/api/stats", {
