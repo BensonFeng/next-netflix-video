a37c7fd243628ef8056359e6de535c4454042459
BensonFeng <bensonfeng@outlook.com>
2022-03-23 20:05:37 -0400

    build my list page

---
+++ pages/browse/my-list.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import NavBar from "../../components/nav/navbar";
+import SectionCards from "../../components/card/section-cards";
+
+const MyList = () => {
+  return (
+    <div>
+      <Head>
+        <title>My list</title>
+      </Head>
+      <main>
+        <NavBar />
+        <div>
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MyList;
